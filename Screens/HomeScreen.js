998a0efa15425709599f7a03b629ad40da0c314a
theRahulKushwaha <therahulkushwaha16@gmail.com>
2024-07-18 07:16:15 +0530

    Updated

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -7,56 +7,183 @@ import {
   Image,
   Touchable,
   TouchableOpacity,
+  TextInput,
+  ScrollView,
 } from 'react-native';
 import HomeScreenStyle from '../Styles/HomeScreenStyle';
 import MapPreview from './MapPreview';
 
 const HomeScreen = ({navigation}) => {
   return (
-    <View>
-      <View style={HomeScreenStyle.Container}>
-        <Text style={HomeScreenStyle.HiPeople}> Hi People,</Text>
+    // \\\\\\\\\\\\\\\\\\\\\\\\\\HomeScreen\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-        <View style={HomeScreenStyle.inline1}>
-          <Text style={HomeScreenStyle.Welcome}>Welcome!</Text>
+    <View style={HomeScreenStyle.Container}>
+      {/* \\\\\\\\\\\\\\\\\\\\\\ HEADER\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  */}
+      <View style={HomeScreenStyle.Header}>
+        <View style={HomeScreenStyle.HeaderContent}>
+          {/* PROFILE IMAGE */}
           <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
             <Image
               source={require('../Assets/man.png')}
               style={HomeScreenStyle.ProfileImage}></Image>
           </TouchableOpacity>
-        </View>
 
-        <TouchableOpacity style={HomeScreenStyle.inline2}>
-          <Image
-            source={require('../Assets/LocationMarker.png')}
-            style={HomeScreenStyle.LocationMarkerIcon}></Image>
-          <Text style={HomeScreenStyle.Address}>Gurugram, Haryana</Text>
-        </TouchableOpacity>
+          {/* HELLO NAME AND LOCATION  */}
+          <View style={HomeScreenStyle.HelloNameLocation}>
+            <Text style={HomeScreenStyle.HelloName}>Hello, Rahul</Text>
 
-        <View style={HomeScreenStyle.WholeMapBox}>
-          <View style={HomeScreenStyle.TopText}>
-            <Text style={HomeScreenStyle.NearByBin}> Nearby bin station </Text>
-            <TouchableOpacity>
-              <Text
-                style={HomeScreenStyle.ViewAll}
-                title="Go to Map"
-                onPress={() => navigation.navigate('MapScreen')}>
-                view all
-              </Text>
-            </TouchableOpacity>
+            <View style={HomeScreenStyle.LocationNameAndContainer}>
+              <Image
+                source={require('../Assets/LocationMarker.png')}
+                style={HomeScreenStyle.LocationMarkerIcon}></Image>
+
+              <Text style={HomeScreenStyle.YourAddress}>Your Location</Text>
+            </View>
           </View>
 
-          <TouchableOpacity
-            style={HomeScreenStyle.MapBox}
-            title="Go to Map"
-            onPress={() => navigation.navigate('MapScreen')}>
-            <MapPreview></MapPreview>
+          {/* NOTIFIACTION BELL ICON  */}
+          <TouchableOpacity>
+            <Image
+              source={require('../Assets/NotificationBellIcon.png')}
+              style={HomeScreenStyle.BellIcon}></Image>
           </TouchableOpacity>
         </View>
 
-        
+        {/* SEARCH BOX AND SCANNER */}
+        <View style={HomeScreenStyle.SearchBoxAndQrIcon}>
+          <TouchableOpacity style={HomeScreenStyle.Searchbox}>
+            <TextInput placeholder="  Search"></TextInput>
+          </TouchableOpacity>
+
+          {/* <Image source={require('../Assets/QrIcon.png')}
+              style={HomeScreenStyle.QrIcon}></Image> */}
+        </View>
+      </View>
+
+      {/* \\\\\\\\\\\\\\\\\\\ OUR SERVICES\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+      <View>
+        <View style={HomeScreenStyle.OurServicesContainer}>
+          <Text style={HomeScreenStyle.OurServicesText}>Our Services</Text>
+
+          <View style={HomeScreenStyle.ServicesBoxes}>
+            <TouchableOpacity style={HomeScreenStyle.ServicesBox}>
+              <Image
+                source={require('../Assets/pickup.png')}
+                style={HomeScreenStyle.ServicesBoxIcons}></Image>
+              <Text style={HomeScreenStyle.ServicesBoxText}>Pick up</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={HomeScreenStyle.ServicesBox}>
+              <Image
+                source={require('../Assets/recycle-bin.png')}
+                style={HomeScreenStyle.ServicesBoxIcons}></Image>
+              <Text style={HomeScreenStyle.ServicesBoxText}>Smart bin</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={HomeScreenStyle.ServicesBox}>
+              <Image
+                source={require('../Assets/gift.png')}
+                style={HomeScreenStyle.ServicesBoxIcons}></Image>
+              <Text style={HomeScreenStyle.ServicesBoxText}>Reward</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={HomeScreenStyle.ServicesBox}>
+              <Image
+                source={require('../Assets/recycle.png')}
+                style={HomeScreenStyle.ServicesBoxIcons}></Image>
+              <Text style={HomeScreenStyle.ServicesBoxText}>Recycle</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* \\\\\\\\\\\\\\\\\\\\\ Recycle MATERIAL \\\\\\\\\\\\\\\\\\\\\\ */}
+        <View style={HomeScreenStyle.RecycleMaterialContainer}>
+          <Text style={HomeScreenStyle.RecycleMaterialText}>
+            Recycle Material
+          </Text>
+
+          <View style={HomeScreenStyle.MateriealContainer}>
+            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={HomeScreenStyle.MateriealType}>
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/plastic.jpeg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Plastic</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/metal.jpg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Metal</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/glass.jpg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Glass</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/newspaper.png')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Newspaper</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/battery.jpg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Battery</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/cardboard.jpg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>Cardboard</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={HomeScreenStyle.MateriealTypeContainer}>
+                <Image
+                  source={require('../Assets/ewaste.jpg')}
+                  style={HomeScreenStyle.MateriealImage}></Image>
+                <Text style={HomeScreenStyle.MateriealText}>E-Waste</Text>
+              </TouchableOpacity>
+
+            </ScrollView>
+          </View>
+        </View>
+
+        {/* \\\\\\\\\\\\\\\\\\\\\\\\\ NEARBY STATION \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+
+        <View style={HomeScreenStyle.WholeBox}>
+
+              <Text style={HomeScreenStyle.NBRSText}>Nearby Recycle Station</Text>
+              <View style={HomeScreenStyle.NBRSContainer}>
+                <View style={HomeScreenStyle.NBRS} >
+
+                  <TouchableOpacity style={HomeScreenStyle.MapContainer}
+                   title="Go to Map"
+                   onPress={() => navigation.navigate('MapScreen')} >
+                    
+                    <MapPreview></MapPreview>
+
+                  </TouchableOpacity>
+                  <Text style={HomeScreenStyle.NBRSInsideText} >
+                  <Text style={HomeScreenStyle.DistanceAndTime}> Distance | Time </Text>
+
+                  </Text>
+                  <Text style={HomeScreenStyle.NBRSAddress}>Address of Nearby station</Text>
+                </View>
+              </View>
+        </View>
       </View>
     </View>
+
+   
   );
 };
 
--- Styles/HomeScreenStyle.js
+++ Styles/HomeScreenStyle.js
@@ -1,82 +1,241 @@
 import {StyleSheet} from 'react-native';
+import {SearchBar} from 'react-native-screens';
+import {Header} from 'react-native/Libraries/NewAppScreen';
 
 const HomeScreenStyle = StyleSheet.create({
-  //--------------------------Header-------------------------------------------------------------
   Container: {
     height: '100%',
-    backgroundColor: '#d7e0de',
-  },
-  HiPeople: {
-    top: '2%',
-    marginLeft: '3.5%',
-  },
-  Welcome: {
-    fontSize: 18,
-    paddingTop: 10,
-    fontWeight: 'bold',
+    width: '100%',
+    backgroundColor: 'white',
+    // alignItems: 'center',
   },
 
-  inline2: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-
-  LocationMarkerIcon: {
-    height: 25,
-    width: 25,
-    marginLeft: 15,
-  },
-
-  Address: {
-    fontWeight: 'bold',
-  },
-
-  inline1: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  Header: {
+    backgroundColor: '#18654a',
+    height: 180,
+    width: '100%',
+    borderBottomRightRadius: 40,
+    borderBottomLeftRadius: 40,
     justifyContent: 'space-between',
-    marginLeft: '4%',
+  },
+
+  HeaderContent: {
+    top: 7,
+    margin: 15,
+    flexDirection: 'row',
   },
 
   ProfileImage: {
     height: 50,
     width: 50,
-    right: 15,
   },
 
-  // -----------------------------------------Map box-------------------------------------------------------
-  WholeMapBox: {
-    height: '32%',
-    width: '90%',
+  HelloNameLocation: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  HelloName: {
+    margin: 4,
+    marginLeft: 10,
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: 'white',
+  },
+
+  LocationNameAndContainer: {
+    flexDirection: 'row',
+    marginLeft: 5,
+  },
+
+  LocationMarkerIcon: {
+    height: 20,
+    width: 20,
+  },
+
+  YourAddress: {
+    fontSize: 13,
+    fontWeight: 'semibold',
+    color: 'white',
+  },
+
+  BellIcon: {
+    height: 35,
+    width: 35,
+  },
+
+  SearchBoxandQrIcon: {
+    // flexDirection:'row',
+  },
+
+  Searchbox: {
+    height: '45%',
+    width: '80%',
     alignSelf: 'center',
-    top: '5%',
+    borderRadius: 40,
+    backgroundColor: 'white',
+    marginTop: '6%',
+    // flexDirection:'row',
   },
 
-  MapBox: {
-    height: '83%',
-    width: '100%',
-    backgroundColor: 'green',
+  QrIcon: {
+    height: 30,
+    width: 30,
+  },
+
+  // \\\\\\\\\\\\\\\\\\\\\\\ OUR SERVICES \\\\\\\\\\\\\\\\\\\\\\\\
+  OurServicesContainer: {
+    top: '3%',
+    height:110,
+    // borderColor:'black',
+    // borderWidth:2,
+
+  },
+
+  OurServicesText: {
+    paddingLeft: '3%',
+    fontSize: 16,
+    fontWeight: 'bold',
+    
+  },
+
+  ServicesBoxes: {
+    height: '65%',
+    width: '95%',
     alignSelf: 'center',
-    top: '5%',
-    borderRadius: 45,
-  },
-
-  TopText: {
+    top: '2%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    // borderColor:'black',
+    // borderWidth:2,
   },
 
-  NearByBin: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  ServicesBox: {
+    height: '100%',
+    width: '18%',
+    // borderColor:'black',
+    // borderWidth:2,
+    backgroundColor: '#18654a',
+    borderRadius: 20,
+    justifyContent: 'center',
   },
 
-  ViewAll: {
-    fontSize: 15,
-    fontWeight: 'bold',
+  ServicesBoxIcons: {
+    height: '50%',
+    width: '50%',
     alignSelf: 'center',
   },
-  // ---------------------------------------
+
+  ServicesBoxText: {
+    alignSelf: 'center',
+    fontSize: 13,
+    color: 'white',
+  },
+
+////////////////////// Recycle MATERIAL \\\\\\\\\\\\\\\\\\\\\\\
+
+ RecycleMaterialContainer:{
+    top:"6%",
+  
+ },
+
+ RecycleMaterialText:{
+  paddingLeft: '3%',
+  fontSize: 16,
+  fontWeight: 'bold',
+ },
+
+ MateriealContainer:{
+    
+ },
+
+ MateriealType:{
+   height:150,
+  //  borderColor:'black',
+  //  borderWidth:2,
+ },
+ 
+ MateriealTypeContainer:{
+ justifyContent:'space-between',
+ marginTop:10,
+ marginLeft:12,
+ marginRight:8,
+ },
+
+ MateriealImage:{
+    height:'85%',
+    width:120,
+  //   borderColor:'black',
+  //  borderWidth:2,
+   borderRadius:20
+
+ },
+
+ MateriealText:{
+    
+    fontSize: 13,
+    color: 'black',
+ },
+
+// \\\\\\\\\\\\\\\\\\\\\\\ NEARBY RECYCLE STATION \\\\\\\\\\\\\\\\\\\\\\
+
+    WholeBox:{
+      top:50,
+      
+      
+    } ,
+
+    NBRSText:{
+        fontSize:16,
+      fontWeight:'bold',
+      marginLeft:"3%",
+    },
+
+  NBRSContainer:{
+  
+      height:200,
+      marginTop:'2%',
+      // borderColor:'black',
+      // borderWidth:2,
+      flexDirection:'row'
+
+  },
+
+  NBRS:{
+    flexDirection:'row',
+    
+  },
+
+   MapContainer:{
+    // borderColor:'black',
+    // borderWidth:2,
+    height:'80%',
+    width:'40%',
+    margin:'3%',
+    borderRadius:30,
+    
+   },
+
+   NBRSInsideText:{
+    // flex:1,
+    // borderColor:'black',
+    // borderWidth:2,
+    top:'7%'
+    
+
+   },
+   
+   DistanceAndTime:{
+    
+    
+    
+   },
+
+   NBRSAddress:{
+    top:'16%',
+    right:'54%',
+    fontSize:14
+   },
 });
 
 export default HomeScreenStyle;
