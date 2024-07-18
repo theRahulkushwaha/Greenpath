1744b11e21d4775148a0ab3bedc496a2c4e964d0
theRahulKushwaha <therahulkushwaha16@gmail.com>
2024-07-18 18:42:07 +0530

    Update

--- Navigation/BottomTabNavigator.js
+++ Navigation/BottomTabNavigator.js
@@ -1,39 +1,111 @@
-import React from "react"; 
-import { View,Text,TouchableOpacity, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native';
 
-import RecycleScreen from "../Screens/RecycleScreen";
-import CamaraScreen from "../Screens/CamaraScreen";
-import WalletScreen from "../Screens/WalletScreen";
-import AccountScreen from "../Screens/AccountScreen";
-import StackNavigator from "./StackNavigator";
+import RecycleScreen from '../Screens/RecycleScreen';
+import CamaraScreen from '../Screens/CamaraScreen';
+import WalletScreen from '../Screens/WalletScreen';
+import AccountScreen from '../Screens/AccountScreen';
+import StackNavigator from './StackNavigator';
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
-        return(
-            <Tab.Navigator 
-            tabBarOption={{
-                showlabel :false,
-                style:{
-                    position:'absolute',
-                    bottom:25,
-                    left:20,
-                    right:20,
-                    elevation:0,
-                    backgroundColor:'black'
-                }
-                }}  >
-
-                <Tab.Screen name="Home" component={StackNavigator}  options={{headerShown: false}}></Tab.Screen>
-                <Tab.Screen name="Recycle" component={RecycleScreen} options={{headerShown: false}}></Tab.Screen>
-                <Tab.Screen name="Camara" component={CamaraScreen} options={{headerShown: false}}></Tab.Screen>
-                <Tab.Screen name="Wallet" component={WalletScreen} options={{headerShown: false}}></Tab.Screen>
-                <Tab.Screen name="Account" component={AccountScreen} options={{headerShown: false}}></Tab.Screen>
-
-            </Tab.Navigator>
-        );
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={StackNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../Assets/Icons/home.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'black',
+                }}></Image>
+            </View>
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Recycle"
+        component={RecycleScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../Assets/Icons/triangular-arrows-sign-for-recycle.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'black',
+                }}></Image>
+            </View>
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Camara"
+        component={CamaraScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../Assets/Icons/camera.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'black',
+                }}></Image>
+            </View>
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Wallet"
+        component={WalletScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../Assets/Icons/wallet.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'black',
+                }}></Image>
+            </View>
+          ),
+        }}></Tab.Screen>
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({focused}) => (
+            <View>
+              <Image
+                source={require('../Assets/Icons/account.png')}
+                resizeMode="contain"
+                style={{
+                  width: 25,
+                  height: 25,
+                  tintColor: focused ? 'red' : 'black',
+                }}></Image>
+            </View>
+          ),
+        }}></Tab.Screen>
+    </Tab.Navigator>
+  );
 };
 
 export default BottomTabNavigator;
