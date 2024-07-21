feaa0ff59d62734d01794757213213fc624519c9
theRahulKushwaha <therahulkushwaha16@gmail.com>
2024-07-22 03:30:08 +0530

    connected firebase

--- Screens/LearnScreen.js
+++ Screens/LearnScreen.js
@@ -1,15 +1,31 @@
-import React from "react";
-import { Text,View } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
 
 const LearnScreen = () => {
-    return(
-        <View>
-            <Text>
-            LearnScreen
+  const [myData, setMyData] = useState(null);
 
-            </Text>
-        </View>
-    );
+  useEffect(() => {
+    getDatabase();
+  }, []);
+
+  const getDatabase = async () => {
+    try {
+      const data = await firestore().collection('users').doc('100').get();
+      setMyData(data.data()); // Use data() method to get the document data
+      console.log(data.data());
+    } catch (error) {
+      console.log(error); // Log the error object
+    }
+  };
+
+  return (
+    <View style={{backgroundColor:'blue'}}>
+      <Text style={{ color: 'red' }}>Name: {myData ? myData.Name : 'Loading....'}</Text>
+      <Text style={{ color: 'red' }}>Age: {myData ? myData.Age : 'Loading....'}</Text>
+      <Text style={{ color: 'red' }}>LearnScreen</Text>
+    </View>
+  );
 };
 
 export default LearnScreen;
